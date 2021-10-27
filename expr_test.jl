fedf9cafde4b73268a81af465b8e6ac30808673c
angus-lewis <angus.lewis@student.adelaide.edu.au>
2021-10-27 22:06:13 +1030

    macro note v1

--- expr_test.jl
+++ expr_test.jl
@@ -1,28 +1,77 @@
-x = 0.0
-w = x
-for n in 1:3
-    w += n
+macro mymac(x)
+    @show x
+    y = 1+x
+    @show y
+    return :($x+$y)
 end
 
 expr = quote 
-    y = x 
-    for n in 1:3
-        y+=n
-    end
-end
-println(eval(expr))
-
-begin 
-    z = x
-    for n in 1:3
-        z += n
-    end
+    @show 3.0
+    y = 1+3.0
+    @show y
+    3.0+y
 end
 
+fun(z) = z + @mymac(3.0)
+gun(z) = begin 
+    @show 3.0
+    y = 1+3.0
+    @show y
+    z + 3.0+y
+end 
+hun(z) = z + eval(expr)
+@code_lowered fun(1)
+@code_lowered gun(1)
+@code_lowered hun(1)
+c = 0
+@macroexpand @mymac(0)
 
+function my_show(expr)
+    val = eval(expr)
+    print(string(expr)*" = ",val)
+end
 
+x=1
+y=2
+my_show(:(z=x+y))
 
-println(x)
-println(y)
-println(z)
-println(w)
+expr = quote 
+    a::Int
+    b::Float64
+end
+st = quote 
+    struct MyT2
+        a::Int
+    end
+end
+Meta.show_sexpr(st)
+
+macro add_generic_fields()
+    return esc(quote
+        one_field::Int
+        another::Int
+        yet_another::Float64
+        a::Vector
+        b::Vector
+        f::Function
+    end)
+end
+
+struct Mt6
+    @add_generic_fields()
+end
+
+macro def(name, definition)
+    return quote
+        macro $(esc(name))()
+            esc($(Expr(:quote, definition)))
+        end
+    end
+end
+
+@macroexpand @def(hi, a::Int)
+@def(hi, a::Int)
+@macroexpand @hi 
+struct MT4
+    @hi
+end
